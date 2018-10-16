b528007e91dc3019afc74c489bf4f09b1683576a
janusz.sek <januszsek0@gmail.com>
2018-10-16 11:10:52 +0200

    Added post method to transaction

--- app/bankapi/app.js
+++ app/bankapi/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require("mysql");
-
+var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var customersRouter = require('./routes/customers');
@@ -12,6 +12,7 @@ var accountsRouter = require('./routes/accounts');
 var transactionsRouter = require('./routes/transactions');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,13 +23,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
 	res.locals.connection = mysql.createConnection({
-		host     : 'db',
-		user     : 'bankappuser',
-		password : 'AppUserPassword',
-		database : 'bankdb'
+		host: 'db',
+		user: 'bankappuser',
+		password: 'AppUserPassword',
+		database: 'bankdb'
 	});
 	res.locals.connection.connect();
 	next();
@@ -41,19 +44,19 @@ app.use('/api/v1/accounts', accountsRouter);
 app.use('/api/v1/transactions', transactionsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- app/bankapi/routes/transactions.js
+++ app/bankapi/routes/transactions.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 	res.locals.connection.query('SELECT * from transactions', function (error, results, fields) {
 		if (error) throw error;
-		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+		res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
 	});
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
 	const id = req.params.id;
 	res.locals.connection.query('SELECT * from transactions where id = ' + id, function (error, results, fields) {
 		if (error) throw error;
-		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+		res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
 	});
+});
+
+router.post('/', function (req, res, next) {
+	const requestBody = req.body;
+	res.locals.connection.query('INSERT INTO transactions (account_id, amount, description, target_iban) VALUES( '
+		+ requestBody.account_id + ', ' + requestBody.amount + ', "' + requestBody.description + '" , ' + requestBody.target_iban
+		+ ' )'
+		, function (error, results, fields) {
+			if (error) throw error;
+			res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+		});
 });
 
 module.exports = router;
