1b32f2884e94f16ffad4761681b76156352bc927
Dawid Buchwald <dawid.buchwald@hotmail.com>
2018-11-04 16:38:09 +0100

    Added distro publish scripts for test app

---
+++ test-docker/distro/build.sh
@@ -0,0 +1,6 @@
+docker tag cgi/hacsec-test-mysql:0.0.1 dawidbuchwald/hacsec-test-mysql:latest
+docker tag cgi/hacsec-test-redis:0.0.1 dawidbuchwald/hacsec-test-redis:latest
+docker tag cgi/hacsec-test-api:0.0.1 dawidbuchwald/hacsec-test-api:latest
+docker tag cgi/hacsec-test-cdn:0.0.1 dawidbuchwald/hacsec-test-cdn:latest
+docker tag cgi/hacsec-test-adminer:0.0.1 dawidbuchwald/hacsec-test-adminer:latest
+docker tag cgi/hacsec-test-frontend:0.0.1 dawidbuchwald/hacsec-test-frontend:latest
---
+++ test-docker/distro/push.bat
@@ -0,0 +1,6 @@
+docker push dawidbuchwald/hacsec-test-mysql:latest
+docker push dawidbuchwald/hacsec-test-redis:latest
+docker push dawidbuchwald/hacsec-test-api:latest
+docker push dawidbuchwald/hacsec-test-cdn:latest
+docker push dawidbuchwald/hacsec-test-adminer:latest
+docker push dawidbuchwald/hacsec-test-frontend:latest
