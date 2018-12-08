e38b752d25b894623815e88ddab8ed0757d35526
Dawid Buchwald <dawid.buchwald@hotmail.com>
2018-12-08 13:57:50 +0100

    Refactored contacts API tests

--- app/bank-api/test/contacts.test.js
+++ app/bank-api/test/contacts.test.js
@@ -1,189 +1,214 @@
-var { USERNAME, UNAUTHORIZED_USERNAME, VALID_PASSWORD, URL } = require('./common');
-
-var chakram = require('chakram');
-var expect = chakram.expect;
-
+var { USERNAME, UNAUTHORIZED_USERNAME, URL, 
+  CONTACTS_URI, METHOD_GET, METHOD_POST, validateHealthCheck, 
+  expectAccessDenied, ensureURLDoesNotExist, logInUser, logOutUser } = require('./common');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var expect = chai.expect;
+chai.use(chaiHttp);
+  
 describe('Contacts API', function() {
 
   before('Validate system healthcheck', function() {
-    return chakram.get(`${URL}/api/v1/health`)
-      .then( response => { 
-        expect(response).to.have.status(200)
-        expect(response).to.comprise.of.json({status:'OK'});
-      })
+    return validateHealthCheck(chai);
   })
 
-  describe('api/v1/contacts GET', function() {
+  describe(`${CONTACTS_URI} GET`, function() {
     it('Should fail when trying to invoke GET on contacts path', function() {
-      var response=chakram.get(`${URL}/api/v1/contacts`);
-      expect(response).to.have.status(404);    
-      return chakram.wait();
+      return ensureURLDoesNotExist(chai, CONTACTS_URI, METHOD_GET);
     })
   })
 
-  describe('api/v1/contacts POST', function() {
+  describe(`${CONTACTS_URI} POST`, function() {
     it('Should fail when trying to invoke POST on contacts path', function() {
-      var response=chakram.post(`${URL}/api/v1/contacts`, {});
-      expect(response).to.have.status(404);    
-      return chakram.wait();
+      return ensureURLDoesNotExist(chai, CONTACTS_URI, METHOD_POST);
     })
   })
 
-  describe('api/v1/contacts/{id} GET', function() {
+  describe(`${CONTACTS_URI}/{id} GET`, function() {
 
     var currentSessionId;
 
     before('Log in to obtain valid session id', function() {
-      return chakram.post(`${URL}/api/v1/login`, {id:USERNAME, password:VALID_PASSWORD})
-        .then( response => { currentSessionId = response.body.sessionId; return expect(response.body.success).to.be.true})
+      return logInUser(chai).then(sessionId => currentSessionId = sessionId);
     })
   
     after('Log out customer to release session', function() {
-      return chakram.post(`${URL}/api/v1/logout`, {}, {headers:{sessionid:currentSessionId}})
-        .then( response => { return expect(response).to.have.status(200)})
+      return logOutUser(chai, currentSessionId);
     })
 
     it('Should list customer contacts correctly when valid session is used', function() {
-      var response=chakram.get(`${URL}/api/v1/contacts/${USERNAME}`, {headers:{sessionid:currentSessionId}});
-      expect(response).to.have.status(200);
-      expect(response).to.have.json( json => {
-        expect(json).to.be.array;
-      })
-      return chakram.wait();
+      return chai.request(URL).get(`${CONTACTS_URI}/${USERNAME}`)
+        .set('sessionid', currentSessionId)
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response).to.be.json;
+          expect(response.body).to.be.an('array');
+        })
     })
 
     it('Should not list customer contacts when unathorized user is used', function() {
-      var response=chakram.get(`${URL}/api/v1/contacts/${UNAUTHORIZED_USERNAME}`, {headers:{sessionid:currentSessionId}});
-      expect(response).to.have.status(401);
-      expect(response).to.comprise.of.json({error: 'Access denied'});
-      return chakram.wait();
+      return chai.request(URL).get(`${CONTACTS_URI}/${UNAUTHORIZED_USERNAME}`)
+        .set('sessionid', currentSessionId)
+        .then(response => expectAccessDenied(chai, response));
+    })
+
+    it('Should fail when invalid session id is provided', function() {
+      return chai.request(URL).get(`${CONTACTS_URI}/${USERNAME}`)
+        .set('sessionid', 'invalid')
+        .then(response => expectAccessDenied(chai, response));
     })
 
     it('Should fail when session id is not provided', function() {
-      var response=chakram.get(`${URL}/api/v1/contacts/${USERNAME}`);
-      expect(response).to.have.status(401);
-      expect(response).to.comprise.of.json({error: 'Access denied'});
-      return chakram.wait();
+      return chai.request(URL).get(`${CONTACTS_URI}/${USERNAME}`)
+        .then(response => expectAccessDenied(chai, response));
     })
   })
 
-  describe('api/v1/contacts/{id} POST', function() {
+  describe(`${CONTACTS_URI}/{id} POST`, function() {
 
     var currentSessionId;
 
     before('Log in to obtain valid session id', function() {
-      return chakram.post(`${URL}/api/v1/login`, {id:USERNAME, password:VALID_PASSWORD})
-        .then( response => { currentSessionId = response.body.sessionId; return expect(response.body.success).to.be.true})
+      return logInUser(chai).then(sessionId => currentSessionId = sessionId);
     })
   
     after('Log out customer to release session', function() {
-      return chakram.post(`${URL}/api/v1/logout`, {}, {headers:{sessionid:currentSessionId}})
-        .then( response => { return expect(response).to.have.status(200)})
+      return logOutUser(chai, currentSessionId);
     })
 
     it('Should create new contact correctly when valid session is used', function() {
-      var response=chakram.post(`${URL}/api/v1/contacts/${USERNAME}`, {name:'Test contact', iban:'PL12 3456 7890'}, {headers:{sessionid:currentSessionId}});
-      expect(response).to.have.status(200);
-      return chakram.wait();
+      return chai.request(URL).post(`${CONTACTS_URI}/${USERNAME}`)
+        .set('sessionid', currentSessionId)
+        .send({name:'Test contact', iban:'PL12 3456 7890'})
+        .then(response => {
+          expect(response).to.have.status(200);
+        })
     })
 
     it('Should not create new contact when unathorized user is used', function() {
-      var response=chakram.post(`${URL}/api/v1/contacts/${UNAUTHORIZED_USERNAME}`, {name:'Test contact', iban:'PL12 3456 7890'}, {headers:{sessionid:currentSessionId}});
-      expect(response).to.have.status(401);
-      expect(response).to.comprise.of.json({error: 'Access denied'});
-      return chakram.wait();
+      return chai.request(URL).post(`${CONTACTS_URI}/${UNAUTHORIZED_USERNAME}`)
+        .set('sessionid', currentSessionId)
+        .send({name:'Test contact', iban:'PL12 3456 7890'})
+        .then(response => expectAccessDenied(chai, response));
     })
 
     it('Should fail when session id is not provided', function() {
-      var response=chakram.post(`${URL}/api/v1/contacts/${USERNAME}`, {name:'Test contact', iban:'PL12 3456 7890'});
-      expect(response).to.have.status(401);
-      expect(response).to.comprise.of.json({error: 'Access denied'});
-      return chakram.wait();
+      return chai.request(URL).post(`${CONTACTS_URI}/${UNAUTHORIZED_USERNAME}`)
+        .send({name:'Test contact', iban:'PL12 3456 7890'})
+        .then(response => expectAccessDenied(chai, response));
+    })
+
+    it('Should fail when invalid session id is provided', function() {
+      return chai.request(URL).post(`${CONTACTS_URI}/${USERNAME}`)
+        .set('sessionid', 'invalid')
+        .send({name:'Test contact', iban:'PL12 3456 7890'})
+        .then(response => expectAccessDenied(chai, response));
     })
   })
 
-  describe('api/v1/contacts/{id}/xml GET', function() {
+  describe(`${CONTACTS_URI}/{id}/xml GET`, function() {
     it('Should fail when trying to invoke GET on contacts XML path', function() {
-      var response=chakram.get(`${URL}/api/v1/contacts/${USERNAME}/xml`);
-      expect(response).to.have.status(404);
-      return chakram.wait();
+      return ensureURLDoesNotExist(chai, `${CONTACTS_URI}/${USERNAME}/xml`, METHOD_GET);
     })
   })
 
-  describe('api/v1/contacts/{id}/xml POST', function() {
+  describe(`${CONTACTS_URI}/{id}/xml POST`, function() {
 
     var currentSessionId;
 
     before('Log in to obtain valid session id', function() {
-      return chakram.post(`${URL}/api/v1/login`, {id:USERNAME, password:VALID_PASSWORD})
-        .then( response => { currentSessionId = response.body.sessionId; return expect(response.body.success).to.be.true})
+      return logInUser(chai).then(sessionId => currentSessionId = sessionId);
     })
   
     after('Log out customer to release session', function() {
-      return chakram.post(`${URL}/api/v1/logout`, {}, {headers:{sessionid:currentSessionId}})
-        .then( response => { return expect(response).to.have.status(200)})
+      return logOutUser(chai, currentSessionId);
     })
 
     it('Should import contacts correctly when valid session is used', function() {
-      var response=chakram.post(`${URL}/api/v1/contacts/${USERNAME}/xml`, {contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban></contact><contact><name>Business Mike</name><iban>PL99 1234 5678</iban></contact></contacts>'}, {headers:{sessionid:currentSessionId}});
-      expect(response).to.have.status(200);
-      return chakram.wait();
+      return chai.request(URL).post(`${CONTACTS_URI}/${USERNAME}/xml`)
+        .set('sessionid', currentSessionId)
+        .send({contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban></contact><contact><name>Business Mike</name><iban>PL99 1234 5678</iban></contact></contacts>'})
+        .then(response => {
+          expect(response).to.have.status(200);
+        })
     })
 
     it('Should fail gracefully when importing invalid contacts XML', function() {
-      var response=chakram.post(`${URL}/api/v1/contacts/${USERNAME}/xml`, {contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban>'}, {headers:{sessionid:currentSessionId}});
-      expect(response).to.have.status(500);
-      expect(response).to.have.json( json => {
-        expect(json.error).to.match(/XML parsing failed, message: /);
-      })
-      return chakram.wait();
+      return chai.request(URL).post(`${CONTACTS_URI}/${USERNAME}/xml`)
+        .set('sessionid', currentSessionId)
+        .send({contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban>'})
+        .then(response => {
+          expect(response).to.have.status(500);
+          expect(response).to.be.json;
+          expect(response.body.error).to.match(/XML parsing failed, message: /);
+        })
     })
 
     it('Should not delete all contacts when XML parsing fails', function() {
-      return chakram.post(`${URL}/api/v1/contacts/${USERNAME}/xml`, {contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban></contact><contact><name>Business Mike</name><iban>PL99 1234 5678</iban></contact></contacts>'}, {headers:{sessionid:currentSessionId}})
-        .then( response => {
+      return chai.request(URL).post(`${CONTACTS_URI}/${USERNAME}/xml`)
+        .set('sessionid', currentSessionId)
+        .send({contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban></contact><contact><name>Business Mike</name><iban>PL99 1234 5678</iban></contact></contacts>'})
+        .then(response => {
           expect(response).to.have.status(200);
-          return chakram.post(`${URL}/api/v1/contacts/${USERNAME}/xml`, {contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban>'}, {headers:{sessionid:currentSessionId}});
         })
-        .then( response => {
+        .then( () => {
+          return chai.request(URL).post(`${CONTACTS_URI}/${USERNAME}/xml`)
+          .set('sessionid', currentSessionId)
+          .send({contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban>'});
+        })
+        .then(response => {
           expect(response).to.have.status(500);
-          return chakram.get(`${URL}/api/v1/contacts/${USERNAME}`, {headers:{sessionid:currentSessionId}});
+          expect(response).to.be.json;
+          expect(response.body.error).to.match(/XML parsing failed, message: /);
         })
-        .then( response => {
+        .then( () => {
+          return chai.request(URL).get(`${CONTACTS_URI}/${USERNAME}`)
+          .set('sessionid', currentSessionId);
+        })
+        .then(response => {
           expect(response).to.have.status(200);
-          expect(response).to.have.json( json => {
-            expect(json).to.be.array;
-            expect(json).to.have.lengthOf(2);
-          })
-          return chakram.wait();            
+          expect(response).to.be.json;
+          expect(response.body).to.be.an('array');
+          expect(response.body).to.have.lengthOf(2);
         })
     })
 
-    it('Should not create new contact when unathorized user is used', function() {
-      var response=chakram.post(`${URL}/api/v1/contacts/${UNAUTHORIZED_USERNAME}/xml`, {contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban></contact><contact><name>Business Mike</name><iban>PL99 1234 5678</iban></contact></contacts>'}, {headers:{sessionid:currentSessionId}});
-      expect(response).to.have.status(401);
-      expect(response).to.comprise.of.json({error: 'Access denied'});
-      return chakram.wait();
+    it('Should not import contacts when unathorized user is used', function() {
+      return chai.request(URL).post(`${CONTACTS_URI}/${UNAUTHORIZED_USERNAME}/xml`)
+        .set('sessionid', currentSessionId)
+        .send({contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban></contact><contact><name>Business Mike</name><iban>PL99 1234 5678</iban></contact></contacts>'})
+        .then(response => expectAccessDenied(chai, response));
+    })
+
+    it('Should fail when invalid session id is provided', function() {
+      return chai.request(URL).post(`${CONTACTS_URI}/${USERNAME}/xml`)
+        .set('sessionid', 'invalid')
+        .send({contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban></contact><contact><name>Business Mike</name><iban>PL99 1234 5678</iban></contact></contacts>'})
+        .then(response => expectAccessDenied(chai, response));
     })
 
     it('Should fail when session id is not provided', function() {
-      var response=chakram.post(`${URL}/api/v1/contacts/${USERNAME}/xml`, {contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban></contact><contact><name>Business Mike</name><iban>PL99 1234 5678</iban></contact></contacts>'});
-      expect(response).to.have.status(401);
-      expect(response).to.comprise.of.json({error: 'Access denied'});
-      return chakram.wait();
+      return chai.request(URL).post(`${CONTACTS_URI}/${USERNAME}/xml`)
+        .send({contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><contacts><contact><name>John Doe</name><iban>PL99 1234 1234</iban></contact><contact><name>Business Mike</name><iban>PL99 1234 5678</iban></contact></contacts>'})
+        .then(response => expectAccessDenied(chai, response));
     })
 
     it('Should not allow XXE to extract /etc/passwd file', function() {
-      return chakram.post(`${URL}/api/v1/contacts/${USERNAME}/xml`, {contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><!DOCTYPE foo [<!ELEMENT foo ANY ><!ENTITY bar SYSTEM "file:///etc/passwd" >]><contacts><contact><name>Business Mike</name><iban>&bar;</iban></contact></contacts>'}, {headers:{sessionid:currentSessionId}})
-        .then( result => {
-          expect(result).to.have.status(200);
-          var response=chakram.get(`${URL}/api/v1/contacts/${USERNAME}`, {headers:{sessionid:currentSessionId}});
+      return chai.request(URL).post(`${CONTACTS_URI}/${USERNAME}/xml`)
+        .set('sessionid', currentSessionId)
+        .send({contactsXml:'<?xml version="1.0" encoding="ISO-8859-1"?><!DOCTYPE foo [<!ELEMENT foo ANY ><!ENTITY bar SYSTEM "file:///etc/passwd" >]><contacts><contact><name>Business Mike</name><iban>&bar;</iban></contact></contacts>'})
+        .then(response => {
           expect(response).to.have.status(200);
-          expect(response).to.have.json( json => {
-            expect(json).to.be.array;
-            expect(json[0].iban).to.not.match(/node:x:1000:1000::\/home\/node:\/bin\/bash/);
-          })
-          return chakram.wait();
+        })
+        .then( () => {
+          return chai.request(URL).get(`${CONTACTS_URI}/${USERNAME}`)
+            .set('sessionid', currentSessionId);
+        })
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response).to.be.json;
+          expect(response.body).to.be.an('array');
+          expect(response.body[0].iban).to.not.match(/node:x:1000:1000::\/home\/node:\/bin\/bash/);        
         })
     })
   })
