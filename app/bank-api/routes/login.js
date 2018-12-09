fc18d76d78c1f18bf61ce2c586e9bfd64a515286
Dawid Buchwald <dawid.buchwald@hotmail.com>
2018-12-09 09:50:05 +0100

    Corrected issue with session listing

--- app/bank-api/routes/login.js
+++ app/bank-api/routes/login.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var { validateCustomerPassword } = require('../common/db/login');
-var { createCustomerSession, getAllCustomersSessions } = require('../common/redis/sessions');
-var { checkIfSessionExists, sendCorrectResult, sendErrorMessage } = require('../common/http/handler');
+var { createCustomerSession } = require('../common/redis/sessions');
+var { sendCorrectResult, sendErrorMessage } = require('../common/http/handler');
 var router = express.Router();
 
 router.post('/', function (req, res) {
@@ -12,14 +12,6 @@ router.post('/', function (req, res) {
     .then( sessionId => sessionId ? {success:true, sessionId} : {success:false, sessionId})
     .then( loginResponse => sendCorrectResult(res, loginResponse) )
     .catch( error => sendErrorMessage(res, error) );
-});
-
-router.get('/sessions', function (req, res) {
-  const sessionId =req.headers.sessionid;
-  checkIfSessionExists(sessionId)
-    .then ( () => getAllCustomersSessions(res.locals.redisClient))
-		.then( results => sendCorrectResult(res, results) )
-		.catch( error => sendErrorMessage(res, error) );
 });
 
 module.exports = router;
