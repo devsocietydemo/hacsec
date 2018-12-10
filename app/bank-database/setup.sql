dcbbadf3c01c29f8454a066f9f86fdfaf13bf13e
Dawid Buchwald <dawid.buchwald@cgi.com>
2018-12-10 10:23:06 +0100

    Corrected order of operations

--- app/bank-database/setup.sql
+++ app/bank-database/setup.sql
@@ -71,8 +71,8 @@ LOAD DATA INFILE '/tmp/customers.csv'
 INTO TABLE customers
 FIELDS TERMINATED BY ','
 (id, name, nationality, @password)
-SET password = SHA2(TRIM(TRAILING '\r' FROM @password), 256),
-    salt = MD5(rand());
+SET salt = MD5(rand()),
+    password = SHA2(TRIM(TRAILING '\r' FROM @password), 256);
 
 LOAD DATA INFILE '/tmp/accounts.csv'
 INTO TABLE accounts
